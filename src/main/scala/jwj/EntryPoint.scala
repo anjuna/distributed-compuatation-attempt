32f3d8eab0db6198b22e535bb1b39143ac886791
Jamie WJ <jamie_wj@yahoo.com>
2019-05-09 11:14:17 +0100

    initial ideas

---
+++ src/main/scala/jwj/EntryPoint.scala
@@ -0,0 +1,21 @@
+package jwj
+
+object EntryPoint {
+
+  //Build up computation in an ADT, like IO/Task
+
+  //Represent worker nodes as in-memory instances of some class
+
+  //maybe actors?
+
+  //use fs2 Stream to fan out computation? serialize the work...
+
+  //Throw in some random failure's to these workers
+
+  //How to implement the DAG scheduler to deal with fault tolerance?
+
+  //Package a jar and invoke main? or something else?
+
+
+
+}
